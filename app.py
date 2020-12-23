01e091eaed8191e20d5ccbd72ec381ea1f12c3c0
Francisco De La Cruz <helloquico@gmail.com>
2020-12-22 21:46:34 -0500

    Create Item Resource

--- app.py
+++ app.py
@@ -9,7 +9,15 @@ items = []
 
 class Item(Resource):
     def get(self, name):
-        return {'item': name}
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'item': None}, 404
+
+    def post(self, name):
+        item = {'name': name, 'price': 4.99}
+        items.append(item)
+        return item, 201
 
 
 api.add_resource(Item, '/item/<string:name>')
