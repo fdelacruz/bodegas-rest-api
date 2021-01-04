a5df1d3f76645b416eda3dbd8854a6bfc97180cb
Francisco De La Cruz <helloquico@gmail.com>
2021-01-04 15:59:48 -0500

    Create the Store Resource

--- models/store.py
+++ models/store.py
@@ -9,12 +9,11 @@ class StoreModel(db.Model):
 
     items = db.relationship('ItemModel', lazy='dynamic')
 
-    def __init__(self, name, price):
+    def __init__(self, name):
         self.name = name
 
     def json(self):
-        return {'name': self.name, 'items': [item.json() for item in
-                                             self.items.all()]}
+        return {'name': self.name, 'items': [item.json() for item in self.items.all()]}
 
     @classmethod
     def find_by_name(cls, name):
