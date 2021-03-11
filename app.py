807aa2632900be6949df43cd1bfd430df5a894fe
Francisco De La Cruz <helloquico@gmail.com>
2021-03-10 22:59:06 -0500

    Activate users manually

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ from resources.user import (
     User,
     UserLogin,
     UserLogout,
-    TokenRefresh
+    TokenRefresh,
+    UserConfirm
 )
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
@@ -104,6 +105,7 @@ api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(UserLogin, '/login')
 api.add_resource(UserLogout, '/logout')
 api.add_resource(TokenRefresh, '/refresh')
+api.add_resource(UserConfirm, '/user_confirm/<int:user_id>')
 
 if __name__ == '__main__':
     from db import db
