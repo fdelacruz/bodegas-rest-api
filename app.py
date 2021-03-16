8e6f3a8ccea2aa5fbf1bd77dc300c24d4d3b41ee
Francisco De La Cruz <helloquico@gmail.com>
2021-03-16 07:44:51 -0400

    Fix app.py

--- app.py
+++ app.py
@@ -7,16 +7,10 @@ from marshmallow import ValidationError
 
 from ma import ma
 
-from resources.user import (
-    UserRegister,
-    User,
-    UserLogin,
-    UserLogout,
-    TokenRefresh,
-    UserConfirm
-)
+from resources.user import UserRegister, User, UserLogin, UserLogout, TokenRefresh
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+from resources.confirmation import Confirmation, ConfirmationByUser
 
 from blacklist import BLACKLIST
 
@@ -105,7 +99,8 @@ api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(UserLogin, '/login')
 api.add_resource(UserLogout, '/logout')
 api.add_resource(TokenRefresh, '/refresh')
-api.add_resource(UserConfirm, '/user_confirm/<int:user_id>')
+api.add_resource(Confirmation, '/user_confirmation/<string:confirmation_id>')
+api.add_resource(ConfirmationByUser, '/confirmation/user/<int:user_id>')
 
 if __name__ == '__main__':
     from db import db
