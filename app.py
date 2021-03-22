4c4eaffee6aa76b41952588f2955adb561386879
Francisco De La Cruz <helloquico@gmail.com>
2021-03-22 19:04:08 -0400

    Retrieve an image

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from resources.user import UserRegister, User, UserLogin, UserLogout, TokenRefre
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from resources.confirmation import Confirmation, ConfirmationByUser
-from resources.image import ImageUpload
+from resources.image import ImageUpload, Image
 from libs.image_helper import IMAGE_SET
 
 from blacklist import BLACKLIST
@@ -105,6 +105,7 @@ api.add_resource(TokenRefresh, '/refresh')
 api.add_resource(Confirmation, '/user_confirmation/<string:confirmation_id>')
 api.add_resource(ConfirmationByUser, '/confirmation/user/<int:user_id>')
 api.add_resource(ImageUpload, "/upload/image")
+api.add_resource(Image, "/image/<string:filename>")
 
 if __name__ == '__main__':
     from db import db
