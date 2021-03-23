6c8266c01636401196ebaccc181b85e33577c9b4
Francisco De La Cruz <helloquico@gmail.com>
2021-03-23 18:48:08 -0400

    Upload users avatar

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from resources.user import UserRegister, User, UserLogin, UserLogout, TokenRefre
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from resources.confirmation import Confirmation, ConfirmationByUser
-from resources.image import ImageUpload, Image
+from resources.image import ImageUpload, Image, AvatarUpload
 from libs.image_helper import IMAGE_SET
 
 from blacklist import BLACKLIST
@@ -106,6 +106,7 @@ api.add_resource(Confirmation, '/user_confirmation/<string:confirmation_id>')
 api.add_resource(ConfirmationByUser, '/confirmation/user/<int:user_id>')
 api.add_resource(ImageUpload, "/upload/image")
 api.add_resource(Image, "/image/<string:filename>")
+api.add_resource(AvatarUpload, "/upload/avatar")
 
 if __name__ == '__main__':
     from db import db
