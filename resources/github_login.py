1ca08ab15ebae64fd2c9cc085878bf1898aee4a3
Francisco De La Cruz <helloquico@gmail.com>
2021-04-01 19:12:50 -0400

    Finish up GithubAuthorize resource

--- resources/github_login.py
+++ resources/github_login.py
@@ -1,6 +1,9 @@
 from flask import g
 from flask_restful import Resource
+from flask_jwt_extended import create_access_token, create_refresh_token
 from oa import github
+
+from models.user import UserModel
 
 
 class GithubLogin(Resource):
@@ -16,4 +19,14 @@ class GithubAuthorize(Resource):
         g.access_token = resp['access_token']
         github_user = github.get('user')
         github_username = github_user.data['login']
-        return github_username
+
+        user = UserModel.find_by_username(github_username)
+
+        if not user:
+            user = UserModel(username=github_username, password=None)
+            user.save_to_db()
+
+        access_token = create_access_token(identity=user.id, fresh=True)
+        refresh_token = create_refresh_token(user.id)
+
+        return {"access_token": access_token, "refresh_token": refresh_token}, 200
